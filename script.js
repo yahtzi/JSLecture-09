dd10e6a583ce813d633bf9abda14b0b8a7a027e9
yahtzi <yaritzabejarano7220@my.whccd.com>
2020-02-10 19:54:02 -0800

    I've added in the second part of the activity, moving on to the third

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-let myDogDouglas = ("Cute", "Precious", "Baby", "Prince", "Demon")
-{
-    console.log(myDogDouglas[0];
-    console.log(myDogDouglas[4]);
-};
+//Instantiate a new array and add 5 numeric values to it then add console.log statements that output first and last values from the array.//
+
+let myDogDouglas = ["Cute", "Precious", "Baby", "Prince", "Demon"];
+
+console.log(myDogDouglas[0]);
+console.log(myDogDouglas[4]);
+
+
+//Using a for loop containing a console.log statement, output each of the values in the array.//
+
+for (d = 0; d <= myDogDouglas.length - 1; d++) {
+    console.log(myDogDouglas[d]);
+}
+
+//Use the push method to add 5 more values to your array then use pop statement 3 times on you r array.//
+
