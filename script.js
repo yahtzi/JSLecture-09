019396d334bb05247c6b7829b6792d12171062c6
yahtzi <yaritzabejarano7220@my.whccd.com>
2020-02-10 20:08:35 -0800

    I've completed the third part of the activity, moving to the last part

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Instantiate a new array and add 5 numeric values to it then add console.log statements that output first and last values from the array.//
 
-let myDogDouglas = ["Cute", "Precious", "Baby", "Prince", "Demon"];
+let myDogDouglas = ["Cute", "Precious", "Baby", "Prince", "Angel"];
 
 console.log(myDogDouglas[0]);
 console.log(myDogDouglas[4]);
@@ -14,3 +14,20 @@ for (d = 0; d <= myDogDouglas.length - 1; d++) {
 
 //Use the push method to add 5 more values to your array then use pop statement 3 times on you r array.//
 
+myDogDouglas.push("Annoying");
+myDogDouglas.push("Demon");
+myDogDouglas.push("Loud");
+myDogDouglas.push("Violent");
+myDogDouglas.push("Cloud");
+
+console.log(myDogDouglas);
+
+myDogDouglas.pop();
+myDogDouglas.pop();
+myDogDouglas.pop();
+myDogDouglas.pop();
+myDogDouglas.pop();
+
+console.log(myDogDouglas);
+
+//Declare a new string variable "myString" and place a string of your choosing inside and output with a console.log statement.//
